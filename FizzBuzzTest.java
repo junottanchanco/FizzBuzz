7ad573dd4bdffffeab1d9eb72d4005be6de487eb
Jesus Tanchanco <junot_t@yahoo.com>
2017-09-15 11:33:00 -0400

    first commit

---
+++ FizzBuzzTest.java
@@ -0,0 +1,9 @@
+public class FizzBuzzTest{
+  public static void main(String[] args){
+    FizzBuzz fB = new FizzBuzz();
+    System.out.println(fB.fizzBuzz(3));
+    System.out.println(fB.fizzBuzz(5));
+    System.out.println(fB.fizzBuzz(15));
+    System.out.println(fB.fizzBuzz(2));
+  }
+}
